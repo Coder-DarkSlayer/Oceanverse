92fb8c1fb95964f9ba873c3fe0b0bb6a6f8e3f59
Ritesh <87757827+Coder-DarkSlayer@users.noreply.github.com>
2023-09-17 15:11:08 +0530

    Update Q56.py

--- Q56.py
+++ Q56.py
@@ -1,8 +1,8 @@
-def partition(lst , end ,start = -1): # partition function
+def partition(lst , end ,start): # partition function
     pivot = lst[end] # pivot element
     while start < end: # loop to find the correct position of pivot
         if lst[start] > pivot: # if element is greater than pivot   
-            lst.append(lst.pop(start)) # move it to the end of list
+            lst.insert(end,lst.pop(start)) # move it to the end of list
             end -= 1 # decrease the end
         else: # if element is smaller than pivot
             start += 1 # increase the start
@@ -14,14 +14,15 @@ def select(lst , start , end , k): # function to find kth smallest element
         pivot = partition(lst , end , start) # find the correct position of pivot
 
         if pivot == k: # if pivot is equal to k
-            return float(lst[pivot]) # return the element at pivot
+            return lst[pivot] # return the element at pivot
         elif pivot > k: # if pivot is greater than k
             return select(lst , start , pivot-1 , k) # call the function for the left part of pivot
         else: # if pivot is smaller than k
             return select(lst , pivot+1 , end , k) # call the function for the right part of pivot
-    else: # if start is greater than end
-        return None # return 0
-        
+
+    elif start == end: # if end and start are same
+        return lst[start] 
+
 def median(l): # function to find median
     if len(l)%2 == 1: # if length of list is odd
         return select(l , 0 , len(l)-1 , len(l)//2) # return the middle element
@@ -29,6 +30,6 @@ def median(l): # function to find median
         return ((select(l , 0 , len(l)-1 , (len(l)-1)//2) + select(l , 0 , len(l)-1 , len(l)//2)))/2 # return the average of middle two elements
 
 if __name__ == "__main__":  # if the module is run directly
-    x = [1, 4, 7, 5, 8, 57, 86, 743, 8, 73,56.5, 84]  # list to find median
+    x = [1,12,43,45,45,21,44]  # list to find median
     # printing the median of the list
     print(f"Median of following list is {median(x)}.")
